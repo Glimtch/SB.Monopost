726402e1878c8642931c48f4cc0ccfb3eda3d229
Glimtch <theglimtch@yahoo.com>
2018-10-26 03:12:11 +0300

    .

--- Dynamics/Plugins/SB.DriverOrder/PostCreate.cs
+++ Dynamics/Plugins/SB.DriverOrder/PostCreate.cs
@@ -21,24 +21,42 @@ namespace SB.DriverOrder
             try
             {
                 var target = (Entity)context.InputParameters["Target"];
-                var multiRequest = new ExecuteMultipleRequest()
-                {
-                    Requests = new OrganizationRequestCollection(),
-                    Settings = new ExecuteMultipleSettings()
-                    {
-                        ContinueOnError = false,
-                        ReturnResponses = false
-                    }
-                };
+
+                //var multiRequest = new ExecuteMultipleRequest()
+                //{
+                //    Requests = new OrganizationRequestCollection(),
+                //    Settings = new ExecuteMultipleSettings()
+                //    {
+                //        ContinueOnError = false,
+                //        ReturnResponses = false
+                //    }
+                //};
+
 
                 foreach(var delivery in GetTopPriorDeliveries(service, target))
                 {
                     delivery["sb_driverorderid"] = new EntityReference("sb_driverorder", target.Id);
 
-                    multiRequest.Requests.Add(new UpdateRequest() { Target = delivery });
-                }
+                    //multiRequest.Requests.Add(new UpdateRequest() { Target = delivery });
 
-                service.Execute(multiRequest);
+                    //Task.Factory.StartNew(() => service.Update(delivery));
+
+                    service.Update(delivery);
+                }
+                
+
+                var postOfficeId = target.GetAttributeValue<EntityReference>("sb_shipfrom_postofficeid").Id;
+                var apcId = target.GetAttributeValue<EntityReference>("sb_shipto_apcid").Id;
+
+                var postOfficeName = service.Retrieve("sb_postoffice", postOfficeId, new ColumnSet("sb_name"))["sb_name"];
+                var apcName = service.Retrieve("sb_automatedpostalcenter", apcId, new ColumnSet("sb_name"))["sb_name"];
+                var date = DateTime.Now.ToString("dd/MM/yyyy");
+
+                target["sb_name"] = $"{ postOfficeName } - { apcName } - {date}";
+
+                service.Update(target);
+
+                //service.Execute(multiRequest);
             }
             catch(Exception e)
             {
@@ -48,15 +66,15 @@ namespace SB.DriverOrder
 
         static IEnumerable<Entity> GetTopPriorDeliveries(IOrganizationService service, Entity driverOrder)
         {
-            var fromPostOfficeId = driverOrder.GetAttributeValue<EntityReference>("sb_shipfrom_postoffice").Id;
-            var toCityId = driverOrder.GetAttributeValue<EntityReference>("sb_shipto_cityid").Id;
+            var fromPostOfficeId = driverOrder.GetAttributeValue<EntityReference>("sb_shipfrom_postofficeid").Id;
+            var toApcId = driverOrder.GetAttributeValue<EntityReference>("sb_shipto_apcid").Id;
 
             var ordersQuery = new QueryExpression("sb_order")
             {
-                ColumnSet = new ColumnSet("sb_orderid", "sb_shipfrom_postofficeid", "sb_shipto_cityid", "sb_orderstatuscode")
+                ColumnSet = new ColumnSet("sb_orderid", "sb_shipfrom_postofficeid", "sb_shipto_apcid", "sb_orderstatuscode")
             };
             ordersQuery.Criteria.AddCondition("sb_shipfrom_postofficeid", ConditionOperator.Equal, fromPostOfficeId);
-            ordersQuery.Criteria.AddCondition("sb_shipto_cityid", ConditionOperator.Equal, toCityId);
+            ordersQuery.Criteria.AddCondition("sb_shipto_apcid", ConditionOperator.Equal, toApcId);
             ordersQuery.Criteria.AddCondition("sb_orderstatuscode", ConditionOperator.Equal, 110000001);
 
             var orders = service.RetrieveMultiple(ordersQuery).Entities;
@@ -70,15 +88,14 @@ namespace SB.DriverOrder
                 deliveryQuery.Criteria.AddCondition("sb_orderid", ConditionOperator.Equal, order.Id);
                 deliveryQuery.Criteria.AddCondition("sb_deliverystatuscode", ConditionOperator.Equal, 110000001);
 
-                Task.Run(() =>
-                {
-                    deliveries.AddRange(service.RetrieveMultiple(deliveryQuery).Entities);
-                });
+                deliveries.AddRange(service.RetrieveMultiple(deliveryQuery).Entities);
             }
 
             var vehicleId = driverOrder.GetAttributeValue<EntityReference>("sb_deliveryvehicleid").Id;
             var vehicle = service.Retrieve("sb_deliveryvehicle", vehicleId, new ColumnSet("sb_capacity"));
-            return deliveries.OrderBy(d => d.GetAttributeValue<DateTime>("sb_datecreated")).Take(vehicle.GetAttributeValue<int>("sb_capacity"));
+            var apc = service.Retrieve("sb_automatedpostalcenter", toApcId, new ColumnSet("sb_freecellscount"));
+            var maxDeliveries = Math.Min(vehicle.GetAttributeValue<int>("sb_capacity"), apc.GetAttributeValue<int>("sb_freecellscount"));
+            return deliveries.OrderBy(d => d.GetAttributeValue<DateTime>("sb_datecreated")).Take(maxDeliveries);
         }
     }
 }
---
+++ Dynamics/SB.Monopost.Workflows/SetDeliveriesPhones.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Messages;
+using Microsoft.Xrm.Sdk.Query;
+using Microsoft.Xrm.Sdk.Workflow;
+using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SB.Monopost.Workflows
+{
+    public sealed partial class SetDeliveriesPhones : CodeActivity
+    {
+        [RequiredArgument]
+        [Input("Order Recipient")]
+        [ReferenceTarget("contact")]
+        public InArgument<EntityReference> Contact { get; set; }
+
+        [RequiredArgument]
+        [Input("Order")]
+        [ReferenceTarget("sb_order")]
+        public InArgument<EntityReference> Order { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            IWorkflowContext workflowContext = context.GetExtension<IWorkflowContext>();
+            IOrganizationServiceFactory serviceFactory = context.GetExtension<IOrganizationServiceFactory>();
+            IOrganizationService service = serviceFactory.CreateOrganizationService(workflowContext.InitiatingUserId);
+
+            var query = new QueryExpression("sb_delivery")
+            {
+                ColumnSet = new ColumnSet(false)
+            };
+            query.Criteria.AddCondition("sb_orderid", ConditionOperator.Equal, Order.Get(context).Id);
+
+            var deliveries = service.RetrieveMultiple(query).Entities;
+
+            //var multiRequest = new ExecuteMultipleRequest()
+            //{
+            //    Requests = new OrganizationRequestCollection(),
+            //    Settings = new ExecuteMultipleSettings()
+            //    {
+            //        ContinueOnError = false,
+            //        ReturnResponses = false
+            //    }
+            //};
+
+            var contactId = Contact.Get(context).Id;
+            var contact = service.Retrieve("contact", contactId, new ColumnSet("telephone1"));
+
+            foreach(var delivery in deliveries)
+            {
+                delivery["sb_phonetonotificate"] = contact.GetAttributeValue<string>("telephone1");
+
+                //Task.Factory.StartNew(() => service.Update(delivery));
+                //multiRequest.Requests.Add(new UpdateRequest() { Target = delivery });
+
+                service.Update(delivery);
+            }
+
+            //service.Execute(multiRequest);
+        }
+    }
+}
